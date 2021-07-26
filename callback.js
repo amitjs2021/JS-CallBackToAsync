638129cdf50f010931164d0876fc2e321cb6df29
amitjs2021 <77134238+amitjs2021@users.noreply.github.com>
2021-07-26 18:26:53 +1000

    test first

---
+++ callback.js
@@ -0,0 +1,5 @@
+//what is call back
+// I will create bacis callback and move to advance level
+// we will learn how and what is the main use of callback
+// we will also learn why we don't need callbacks and is there any better approach
+console.log("Hello World");
